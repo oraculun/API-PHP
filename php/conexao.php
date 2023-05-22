bc5d24a43d972222481e96ee89fe4b9b5e796ab5
Luciano Passarelli <passarellidev@gmail.com>
2023-05-22 10:01:17 -0300

    Initial commit

---
+++ php/conexao.php
@@ -0,0 +1,12 @@
+<?php
+
+//variáveis
+$ulr = "localhost";
+$usuario = "root";
+$senha = "";
+$base = "api";
+
+//conexão
+$conexao = new mysqli($ulr,$usuario,$senha,$base);
+
+?>
---
+++ php/listar.php
@@ -0,0 +1,28 @@
+<?php
+
+//Incluir a conexão
+include("conexao.php");
+
+//SQL
+$sql = "SELECT * FROM cursos";
+
+//
+$executar = mysqli_query($conexao,$sql);
+
+//Vetor
+$cursos = [];
+
+//Índice
+$indice = 0;
+
+//Laço
+while($linha = mysqli_fetch_assoc($executar)){
+    $cursos[$indice]['idCurso'] = $linha['idCurso'];
+    $cursos[$indice]['nomeCurso'] = $linha['nomeCurso'];
+    $cursos[$indice]['valorCurso'] = $linha['valorCurso'];
+
+    $indice++;
+}
+
+json_encode(['dados'=>$cursos]);
+?>
