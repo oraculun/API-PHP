21c26d9993eb56c08fe66dad565658ce9d2f7996
Luciano Passarelli <passarellidev@gmail.com>
2023-05-22 13:58:22 -0300

    criado o componente curso

--- app-php/src/app/app.module.ts
+++ app-php/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { CursoComponent } from './curso/curso.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CursoComponent
   ],
   imports: [
     BrowserModule,
