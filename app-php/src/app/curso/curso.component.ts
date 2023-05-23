a40992619dbf5b4ab4782ed5b305ab49af2eca14
Luciano Passarelli <passarellidev@gmail.com>
2023-05-23 10:40:39 -0300

    atualizado o arquivo curso.component.ts
    Inicializado a criação das funções CRUD

--- app-php/src/app/curso/curso.component.ts
+++ app-php/src/app/curso/curso.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-curso',
   templateUrl: './curso.component.html',
   styleUrls: ['./curso.component.css']
 })
-export class CursoComponent {
+export class CursoComponent implements OnInit {
 
+  //construtor
+  constructor(){ }
+
+  //Inicializador
+  ngOnInit(){
+  }
+  //Cadastro
+  cadastro():void{
+    alert("Cadastro");
+  }
+
+  //Seleção
+  selecao():void{
+    alert("Seleção");
+  }
+
+  //Alterar
+  alterar():void{
+    alert("Alterar");
+  }
+
+  //Remover
+  remover():void{
+    alert("Remover");
+  }
 }
