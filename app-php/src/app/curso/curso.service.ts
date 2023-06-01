194ac9f417848ba7e045799704c95fe327db0f3d
Luciano Passarelli <passarellidev@gmail.com>
2023-06-01 15:48:54 -0300

    Criado o método de atualização no curso.service.ts

--- app-php/src/app/curso/curso.service.ts
+++ app-php/src/app/curso/curso.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { filter, map } from 'rxjs/operators';
 import { Curso } from './curso';
-import { Observable } from 'rxjs';
+import { Observable, pipe } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -28,23 +28,36 @@ export class CursoService {
     )
   }
 
-  /*
   //Cadastrar curso
-  cadastrarCurso(c:Curso): Observable<Curso[]>{
-    return this.http.post(this.url+'cadastrar', {cursos:c}) //enviar os dados cadastrar.php
-    .pipe(map((res:any) => {//percorre as informações
-      this.vetor.push(res['cursos']); //insere a resposta de cursos
-      return this.vetor;
-    }))
-  }
-  */
-   //Cadastrar curso
   cadastrarCurso(c:Curso):Observable<Curso>{
     return this.http.post<Curso>(this.url+'cadastrar', c);
   }
 
+  //Remover curso
   removerCurso (id:any):Observable<Curso>{
     const url = `${this.url}excluir?idCurso=${id}`;
     return this.http.delete<Curso>(url);
-    }
+  }
+
+  //Atualizar curso
+  atualizarCurso(c:Curso):Observable<Curso[]>{
+
+    //Executar a alteração via URL
+    return this.http.put(this.url+'alterar', {cursos: c})
+    //Percorer o vetor para saber qual é o id do curso alterado
+    .pipe(map((res) => {
+      const cursoAlterado = this.vetor.find((item) => {
+        return +item['idCurso'] === +['idCurso'];
+      });
+
+      //Alterar o valor do vetor local
+      if(cursoAlterado){
+        cursoAlterado['nomeCurso'] = c['nomeCurso'];
+        cursoAlterado['valorCurso'] = c['valorCurso'];
+      }
+
+      //Retorno
+      return this.vetor;
+    }))
+  }
 }
